cc62468c6b6afe8fc26447d493fdbed89fec323e
krfagu17 <krfagu17@gmail.com>
2024-05-13 06:49:11 +0530

    all done for deployment

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def index():
 def get_file(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
